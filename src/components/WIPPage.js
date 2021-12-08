c658cf9019fbc599f013554c80396b2084fd34d3
Antonín Šimek <ansimek@csas.cz>
2021-12-08 19:36:26 +0100

    ASI: WIP Page animations + index

--- src/components/WIPPage.js
+++ src/components/WIPPage.js
@@ -3,14 +3,13 @@ function WIPPage() {
     return (
         <div class="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12">
             <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-                <div class="absolute inset-0 bg-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+                <div class="absolute inset-0 bg-gradient-to-tl from-blue-400 to-blue-800 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl animate-pulse"></div>
                 <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
                     <div class="max-w-md mx-auto">
-                        <div>
-                            <p class="text-xl text-gray-700">
-                                Anthony Šimek - Personal website
-                            </p>
-                        </div>
+                        <p class="text-xl text-gray-700">
+                            Anthony Šimek - Personal website
+                        </p>
+
                         <div class="divide-y divide-gray-200">
                             <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                 <p>This project is currently in progress</p>
@@ -19,7 +18,7 @@ function WIPPage() {
                             <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
                                 <p>Want to see other projects that I'm working on?</p>
                                 <p>
-                                    <a href="https://github.com/TonnyCS" class="text-cyan-600 hover:text-cyan-700"> GitHub &rarr; </a>
+                                    <a href="https://github.com/TonnyCS" class="text-blue-400 hover:text-blue-800 transition-colors"> GitHub &rarr; </a>
                                 </p>
                             </div>
                         </div>
